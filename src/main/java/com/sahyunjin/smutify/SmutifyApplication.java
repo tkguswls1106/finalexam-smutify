811723550102020cf63d0e1af2730d6c1cc6a4d7
sahyunjin <tkguswls1106@naver.com>
2023-12-16 03:23:15 +0900

    war 빌드 버전으로 코드 변경.

--- src/main/java/com/sahyunjin/smutify/SmutifyApplication.java
+++ src/main/java/com/sahyunjin/smutify/SmutifyApplication.java
@@ -4,16 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SmutifyApplication {
+public class SmutifyApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmutifyApplication.class, args);
 	}
 
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//		return application.sources(SmutifyApplication.class);
-//	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SmutifyApplication.class);
+	}
 }
